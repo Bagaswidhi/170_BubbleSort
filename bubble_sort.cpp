7af766965fd836444f02101827d6c5b06b987842
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-03-13 11:08:40 +0700

    Membuat porsedur display, dan memanggil prosedur input

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -52,10 +52,24 @@ void bubble_SortArray()
     }while (pass <= n-1); // Step 5    
 }
 
+void display()
+{
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun " << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j=0 ; j < n ; j++)
+    {
+        cout << arr[j];
+        if (j<n-1)
+        {
+            cout << "-->";
+        }
+    }
+}
+
 int main()
 {
-
-
-
-
+    input();
 }
