943d7f7329b1bb5521d96a465b22db3d089cccc7
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-03-13 10:55:40 +0700

    first commit

---
+++ bubble_sort.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20]; // Deklarasi Variable Global array dengan panjang 20
+int n; // Deklarasi Variable Global n untuk menyimpan elemen pada array
+
+void input() //Procedure input
+{
+    int d;
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Array maksimal adalah 20" << endl;
+        }
+    }
+    cout << endl;
+    cout << "==========================" << endl;
+    cout << "Masukkan elemen pada array" << endl;
+    cout << "==========================" << endl;
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> arr[i];
+    }
+    
+}
+
+void bubble_SortArray()
+{
+    int pass = 1; // Step 1
+    do{
+        for (int j = 0;j <= n - 1 - pass ;j++) // Step 2
+        {
+            if (arr[j] > arr[j+1]) // Step 3
+            {
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+        pass++; // Step 4
+    }while (pass <= n-1); // Step 5    
+}
+
+int main()
+{
+
+
+
+
+}
